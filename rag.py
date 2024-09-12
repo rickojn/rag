a3e6cbef96a5e72c2ab4d264b35bf62c8b7c7aca
rickojn <rickojn@hotmail.com>
2024-09-12 22:32:21 +0100

    wip

--- rag.py
+++ rag.py
@@ -1,15 +1,25 @@
 import os
 import docx
+from sentence_transformers import SentenceTransformer, util
 
-def extract_text_from_docx(docx_path):
+def extract_paragraphs_from_docx(docx_path):
     doc = docx.Document(docx_path)
     full_text = []
     for paragraph in doc.paragraphs:
         full_text.append(paragraph.text)
-    return '\n'.join(full_text)
+    return full_text
+
+def embed_paragraphs(paragraphs, model):
+    return model.encode(paragraphs, convert_to_tensor = True)
+
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+paragraphs = extract_paragraphs_from_docx("input-docs/Employment-Contract-08-21-01.docx")
+
+paragraph_embeddings = embed_paragraphs(paragraphs, model)
+
+print(paragraphs[0])
+print(paragraph_embeddings[0].shape())
 
 
 
 
-text = extract_text_from_docx("input-docs/Employment-Contract-08-21-01.docx")
-print(text )
