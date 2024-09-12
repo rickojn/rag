a7d20d7bcc5bb7aa59eedbccb931992ef0d7e406
rickojn <rickojn@hotmail.com>
2024-09-12 22:49:01 +0100

    wip

--- rag.py
+++ rag.py
@@ -10,7 +10,7 @@ def extract_paragraphs_from_docx(docx_path):
     return full_text
 
 def embed_paragraphs(paragraphs, model):
-    return model.encode(paragraphs, convert_to_tensor = True)
+    return model.encode(paragraphs, convert_to_tensor = True, show_progress_bar = True)
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 paragraphs = extract_paragraphs_from_docx("input-docs/Employment-Contract-08-21-01.docx")
@@ -18,7 +18,7 @@ paragraphs = extract_paragraphs_from_docx("input-docs/Employment-Contract-08-21-
 paragraph_embeddings = embed_paragraphs(paragraphs, model)
 
 print(paragraphs[0])
-print(paragraph_embeddings[0].shape())
+print(paragraph_embeddings[0].shape)
 
 
 
